1aad9920f6e71783cde4edb87ae412c4368e733d
Moiz Uddin M. Abdullah <moiz211j@gmail.com>
2024-04-25 16:44:45 +0500

    Initial Working of signup page without design

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WebProject
+
+# Links used:
+## Free SVG Backgrounds: https://www.svgbackgrounds.com/set/animated-svg-backgrounds/
--- front-end1/src/app/components/Signup.js
+++ front-end1/src/app/components/Signup.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import ShowData from './ShowData';
-import 'C:/Users/moiz2/Desktop/6th Sem/WEB/Final Project/Practice Projects/2.0/front-end1/src/app/globals.css'
 
 export default function Signup() {
 
   const API_BASE = 'http://localhost:4002/users';
-  const [items, setItems] = useState([]);
-  const [id, setId] = useState('');
-  const [password, setpassword] = useState('');
 
+  const [id, setId] = useState('');
+  
+  const [password, setpassword] = useState('');
+  
+  const [phoneno,setPhoneno] = useState('');
+
+  const [FirstName,setFirstName] = useState('');
+
+  const [LastName,setLastName] = useState('');
+
+  const handleLastNameChange = (event) =>{
+    setLastName(event.target.value);
+  }
+
+  const handleFirstNameChange = (event) =>{
+    setFirstName(event.target.value);
+  }
+
+
+  const handlePhoneChange = (event) =>{
+    setPhoneno(event.target.value);
+  }
   const handleIdChange = (event) => {
     setId(event.target.value);
   };
@@ -26,7 +44,7 @@ export default function Signup() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ id, password }),
+        body: JSON.stringify({ id, password, phoneno }),
       });
     
       if (response.ok) {
@@ -38,31 +56,49 @@ export default function Signup() {
     }catch(error){
       console.log('error');
     }
-  ;
+  };
   
 
   return (
-    <div>
-      <div className='Signup-page'>
-        <div className='container signup-cont'>
-          <div className="form-floating mb-3">
-            <input type="text" className="form-control" id="id" placeholder="ID" value={id} onChange={handleIdChange}/>
-            <label htmlFor="id">ID</label>
-          </div>
-          <div className="form-floating">
-            <input type="passwordword" className="form-control" id="password" placeholder="passwordword" value={password} onChange={handlepasswordChange}/>
-            <label htmlFor="password">passwordword</label>
+
+      <div class="Main-Container">
+        <div class="Image-left">
+          <h1>Welcome to Contribution system</h1>
+        </div>
+
+        <div class="Signup-page">
+          <div class="container Signup-Form-Design">
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="fname" placeholder="First Name" value={FirstName} onChange={handleFirstNameChange}/>
+              <label for="fname">First Name</label>
+            </div>
+
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="lname" placeholder="Last Name" value={LastName} onChange={handleLastNameChange}/>
+              <label for="lname">Last Name</label>
+            </div>
+
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="id" placeholder="ID" value={id} onChange={handleIdChange}/>
+              <label for="id">ID</label>
+            </div>
+
+            <div class="form-floating mb-3">
+              <input type="password" class="form-control" id="password" placeholder="password" value={password} onChange={handlepasswordChange}/>
+              <label for="password">password</label>
+            </div>
+
+            <div class="form-floating mb-3">
+              <input type="tel" id="phone" class="form-control" name="phone" placeholder="Phone" value={phoneno} onChange={handlePhoneChange} pattern="\+[0-9]{2}-[0-9]{3}-[0-9]{6}"  />
+              <label for="phone">Phone No:</label>
+            </div>
+
+            <button onClick={handleSubmit}>Send</button>
           </div>
         </div>
-        <button onClick={handleSubmit}>Send</button>
       </div>
 
-      {/* <div>  
-          {Array.isArray(items) && items.map((item)=> {
-            const {_id, name} = item
-            return  <ShowData key={_id} name={name} id={_id} setItems={setItems}/>   
-          })}
-      </div> */}
-    </div>
+
+
   );
 }
